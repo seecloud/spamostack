b5a8ba05f0ab0c5f06abdb76384cf03a0fee0b62
Mikhail Ivanov <mivanov@mirantis.com>
2016-09-26 20:19:25 +0300

    Update cache saving/loading

--- spamostack/cache.py
+++ spamostack/cache.py
@@ -15,49 +15,49 @@ class Cache(object):
 
         self.db = leveldb.LevelDB(path)
         self.cache = dict()
-        default_init()
+        self.default_init()
         self.client_factory = ClientFactory()
         self.load()
 
     def default_init(self):
-        self.cache["created"]["users"][os.environ['OS_USERNAME']]['password'] = os.environ['OS_PASSWORD']
-        self.cache["created"]["users"][os.environ['OS_USERNAME']]['project_name'] = os.environ['OS_TENANT_NAME']
+        "Default initialization for cache"
+
+        uname = os.environ['OS_USERNAME']
+
+        self.cache["created"]["users"][uname]['password'] = \
+            os.environ['OS_PASSWORD']
+        self.cache["created"]["users"][uname]['project_name'] = \
+            os.environ['OS_TENANT_NAME']
         self.cache["auth_url"] = os.environ['OS_AUTH_URL']
-        self.cache["created"]["users"][os.environ['OS_USERNAME']]['project_domain_id'] = 'default'
-        self.cache["created"]["users"][os.environ['OS_USERNAME']]['user_domain_id'] = 'default'
-
-    def convert_str_to_obj(self, name, cache_elem):
-        """
-
-        :param name:
-        :param cache_elem:
-        :return:
-        """
-        client = getattr(self.client_factory, name)
-        for i in cache_elem.split(','):
-            if i.startswith("id="):
-                idx = i.split('=')[1]
-
-        obj = client.get(id=idx)
-
-        return obj
+        self.cache["created"]["users"][uname]['project_domain_id'] = 'default'
+        self.cache["created"]["users"][uname]['user_domain_id'] = 'default'
 
     def load(self):
         """Load db into cache"""
-	def loop(tasks, name):
-            for key, value in tasks.iteritems():
+
+        def to_objects(cache_elem, client_name):
+            for key, value in cache_elem.iteritems():
                 if isinstance(value, dict):
-                    loop(value, name)
+                    to_objects(value, client_name)
                 else:
-                    tasks[key] = [convert_str_to_obj(name, el) for el in value]
+                    cache_elem[key] = [getattr(self.client_factory, client_name).get(id=el)
+                                       for el in value]
 
         for key, value in self.db.RangeIter():
-            loop(eval(value), key)
+            to_objects(eval(value), key)
 
     def update(self):
         """Update existing db with data from cache"""
 
+        def to_strings(cache_elem):
+            for key, value in cache_elem.iteritems():
+                if isinstance(value, dict):
+                    to_strings(value)
+                else:
+                    cache_elem[key] = [str(el.id) for el in value]
+
         batch = leveldb.WriteBatch()
         for key, value in self.cache.iteritems():
+            to_strings(value)
             self.db.Put(key, str(value))
         self.db.Write(batch, sync=True)
