2a79bb9be9801e5d279b5c2fe68e26522425284f
Mikhail Ivanov <mivanov@mirantis.com>
2016-12-19 17:45:23 +0400

    Update keeper module
    - Added static method for validating uuid4
    - Added simple check, that continues keeping
      default flavors in openstack without deleting
    - Remove strong constraint. Reason:
      https://github.com/openstack-dev/pbr/blob/e0d26741/pbr/packaging.py#L915

--- spamostack/logger.py
+++ spamostack/logger.py
@@ -16,16 +16,6 @@
 import logging
 
 
-class SpamFileHandler(logging.FileHandler):
-
-    def __init__(self):
-        logging.FileHandler.__init__(self, "RM.log")
-        fmt = '%(asctime)s %(filename)s %(levelname)s: %(message)s'
-        fmt_date = '%Y-%m-%dT%T%Z'
-        formatter = logging.Formatter(fmt, fmt_date)
-        self.setFormatter(formatter)
-
-
 class SpamStreamHandler(logging.StreamHandler):
 
     def __init__(self):
